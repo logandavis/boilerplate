a01b05ee73af0a388d82b086e20614590f757d48
Logan Davis <logankeahidavis@gmail.com>
2017-01-26 21:56:06 -0500

    Adds boilerplate code from OlinJS Express tutorial
    Initial commit for boilerplate repository, using code from
    https://github.com/olinjs/olinjs/tree/master/lessons/02-express-templating.

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var home = function(req, res){
+  res.render("home");
+};
+
+var school = function(req, res){
+  res.render("olin", {"classes": [
+    {name:"Olin.js", teacher:"Bill Wong and Cynthia Chen"},
+    {name:"Tell the Story of What You Make", teacher:"Tim Sauder"},
+    {name:"Six Books that Changed the World", teacher:"Rob Martello"},
+    {name:"Entrepreneurship Capstone", teacher:"Lawrence Neeley"},
+    {name:"Senior Capstone Program in Engineering", teacher:"Alexandra Coso Strong"}
+    ]
+  });
+};
+
+module.exports.home = home;
+module.exports.school = school;
